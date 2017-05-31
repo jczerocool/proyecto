fc99b748bc0b0ac3756d369434eb627ccb6c39a4
Juan <jcendersgame@gmail.com>
2017-05-30 17:01:16 -0700

    update2

--- smartlist/settings.py
+++ smartlist/settings.py
@@ -151,8 +151,8 @@ STATIC_URL = '/static/'
 
 if DEBUG:
     MEDIA_URL = '/media/'
-    STATIC_ROOT =   os.path.join(os.path.dirname(BASE_DIR), "static", "static-only")
-    MEDIA_ROOT =   os.path.join(os.path.dirname(BASE_DIR), "static", "media")
+    STATIC_ROOT =   os.path.join(BASE_DIR,  "static", "static-only")
+    MEDIA_ROOT =   os.path.join(BASE_DIR, "static", "media")
     STATICFILES_DIRS = (
     os.path.join(os.path.dirname(BASE_DIR), "static", "static"),
     )
